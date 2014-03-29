a68bd07a40a7d7728e416f06af98b6acad0986e1
mjkint <michaelkint@live.be>
2014-03-29 11:30:46 +0100

    Notifications

--- app/src/main/java/com/sytycc/sytycc/app/MainActivity.java
+++ app/src/main/java/com/sytycc/sytycc/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.sytycc.sytycc.app;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,12 +21,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showNotification(R.drawable.abc_cab_background_top_holo_light,"Notificatie","Pieter Reuse is een homo");
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -35,4 +45,42 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressLint("NewApi")
+    public void showNotification(int imageId, String title, String text) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(imageId)
+                        .setContentTitle(title)
+                        .setContentText(text);
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+
+        PendingIntent resultPendingIntent;
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB){
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(MainActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+        } else {
+            resultPendingIntent = PendingIntent.getActivity(getApplicationContext(),0,getIntent(),0);
+        }
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
+    }
 }
