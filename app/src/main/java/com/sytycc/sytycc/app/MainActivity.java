6a1607f704d715a0eb190a39db17de1480f94fb2
rmangels <mangelschotsroel@hotmail.com>
2014-03-29 13:39:01 +0100

    gui eerste push

--- app/src/main/java/com/sytycc/sytycc/app/MainActivity.java
+++ app/src/main/java/com/sytycc/sytycc/app/MainActivity.java
@@ -4,14 +4,55 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+
+import com.sytycc.sytycc.app.data.Product;
+import com.sytycc.sytycc.app.layout.products.ProductAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TabHost tabHost;
+    private ListView productsListView;
+    private ProductAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Product> productList = new ArrayList<Product>();
+        productList.add(new Product("Cuenta NÓMINA","14650100911708338319",1465,"ES65 1465 0100 91 1708338319","INGDESMMXXX","15/04/2013",17,1,28999,28999));
+
+
+        productAdapter = new ProductAdapter(this,productList);
+
+        productsListView = (ListView) findViewById(R.id.productsListView);
+        productsListView.setAdapter(productAdapter);
+
+        tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1=tabHost.newTabSpec("Home");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Home");
+
+        TabHost.TabSpec spec2=tabHost.newTabSpec("Notifications");
+        spec2.setIndicator("Notifications");
+        spec2.setContent(R.id.tab2);
+
+        TabHost.TabSpec spec3=tabHost.newTabSpec("Settings");
+        spec3.setContent(R.id.tab3);
+        spec3.setIndicator("Settings");
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
     }
 
 
